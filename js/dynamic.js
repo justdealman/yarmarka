ac38c9a5f37af7f85688800bbc52708479326e5a
Alexandr <justdealman@gmail.com>
2015-12-11 11:17:31 +0700

    original

---
+++ js/dynamic.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function() {
+	if ( $('.slider').length > 0 ) {
+		$('.slider').slides({
+			generatePagination: false,
+			generateNextPrev: true,
+			container: 'container',
+			effect: 'slide',
+			slideSpeed: 500,
+			slideEasing: 'easeInOutQuad',
+			play: 10000,
+			pause: 2500,
+		});
+		$('.slider').bind('swipeleft', function() {
+			$('.slider .next').trigger('click');
+		});
+		$('.slider').bind('swiperight', function() {
+			$('.slider .prev').trigger('click');
+		});
+	}
+	if ( $('.gallery-c').length > 0 ) {
+		$('.gallery-c ul').jcarousel({
+			scroll: 1,
+			animation: 500,
+			easing: 'easeInOutQuad',
+			wrap: 'circular'
+		});
+		$('.gallery-c .jcarousel-container').bind('swipeleft', function() {
+			$('.gallery-c .jcarousel-container .jcarousel-next').trigger('click');
+		});
+		$('.gallery-c .jcarousel-container').bind('swiperight', function() {
+			$('.gallery-c .jcarousel-container .jcarousel-prev').trigger('click');
+		});
+	}
+	$('input, textarea').each(function() {
+		$(this).data('holder', $(this).attr('placeholder'));
+		$(this).focusin(function() {
+			$(this).attr('placeholder', '');
+		});
+		$(this).focusout(function() {
+			$(this).attr('placeholder', $(this).data('holder'));
+		});
+	});
+});
---
+++ js/ie8.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+	$('.index .news ul li:nth-child(2n), .benefits-l ul li:nth-child(3n), .renter .benefits ul li:nth-child(3n), .gallery-b li:nth-child(3n), .news-b li:nth-child(2n), .prod-b > div > div:nth-child(3n), .shop-l ul li:nth-child(3n)').css({
+		'margin-right': '-20px'
+	});
+	$('.index .special ul li:nth-child(2n)').css({
+		'margin-right': '-3px'
+	});
+	$('.prod-b > div > div ul li:nth-child(6n)').css({
+		'margin-right': '-10px'
+	});
+});
